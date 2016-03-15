c8a8ba2f3cd84920eac93265dd308d16c80962c8
Daniel McBryde <mcbryded@gmail.com>
2016-03-15 17:11:47 -0500

    complete locksmith

--- src/classes.js
+++ src/classes.js
@@ -1,6 +1,63 @@
 class Foo {
 
-
 }
 
 var bar = new Foo();
+
+class Dog {
+  constructor(says) {
+    this.says = 'life is ruff'
+  }
+}
+var dog = new Dog;
+
+class Cat {
+
+  constructor(garfield) {
+    this.garfield = garfield
+    this.growl = function(growl) {
+      return 'meow'
+    }
+  }
+}
+
+var garfield = new Cat();
+
+class FacelessMan {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+var aMan = new FacelessMan(`Jaqen H'ghar`);
+
+class Lion extends Cat {
+  constructor() {
+    super();
+    this.size = 'BIG';
+  }
+}
+
+
+class KeepSecret {
+  constructor() {
+    this.squeal = function() {
+      return `My class rocks!`
+    }
+  }
+}
+
+class Key {
+  constructor() {}
+}
+class Safe {
+  constructor(secret, keyhole) {
+    this.unlock = function(attempt) {
+      if (attempt === keyhole) {
+        return secret;
+      }
+
+      return undefined;
+    }
+  }
+}
